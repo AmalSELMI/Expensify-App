a40f874367c67a2b5f39f203812c14ba497d58b8
AmalSELMI <amal.selmi@crossquantum.com>
2018-04-09 15:02:05 +0200

    react-router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
