d0806b0662f093b5a0246a4c1e799d340f22abc9
AmalSELMI <amal.selmi@crossquantum.com>
2018-04-11 14:16:01 +0200

    redux part 1

---
+++ src/playground/redux-101.js
@@ -0,0 +1,62 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = { count: 0 }, action) => {
+
+    switch (action.type) {
+      case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+        return {
+          count: state.count + incrementBy
+        };
+      case 'DECREMENT':
+      const decrementBy =  typeof action.decrementBy === 'number' ? action.decrementBy : 1
+        return {
+            count: state.count - decrementBy
+          };
+       case 'RESET':
+        return {
+            count: state.count = 0
+        };
+        case 'SET':
+        return {
+            count: action.count
+        };
+        default:
+        return state;
+    }
+});
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+});
+
+store.dispatch ({
+    type: 'INCREMENT'
+});
+
+
+store.dispatch ({
+    type: 'INCREMENT',
+    incrementBy : 5
+});
+
+
+store.dispatch ({
+    type: 'RESET'
+});
+
+store.dispatch ({
+    type: 'DECREMENT'
+});
+
+store.dispatch ({
+    type: 'DECREMENT',
+    decrementBy : 10
+});
+
+store.dispatch ({
+    type: 'SET',
+    count : 101
+});
+
+// unsubscribe();
