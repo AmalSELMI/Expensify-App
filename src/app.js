45bf7b2fbf43ff516bea43ba962cb5d48df0f641
AmalSELMI <amal.selmi@crossquantum.com>
2018-07-09 11:56:10 +0200

    rendering individual expenses

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(()=> {
+store.dispatch(setTextFilter('b'));
+}, 3000)
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
