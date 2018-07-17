1a23f6603fecc78e17df7c444a3ed566764394a7
AmalSELMI <amal.selmi@crossquantum.com>
2018-07-17 18:11:39 +0200

    enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
-const renderer = new ReactShallowRenderer();
-renderer.render(<Header/>);
-expect(renderer.getRenderOutput()).toMatchSnapshot();
-
-
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+});
